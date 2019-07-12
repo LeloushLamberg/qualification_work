bf5dc0b2e5cab4450081d7423487372de9b77c7a
LeloushLamberg <egor.bil.1987@yandex.ru>
2019-07-13 01:17:45 +0800

    index for section price

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
-$(document).ready (function(){
-$('.hero-slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
-});
-}
+$(document).ready(function () {
+      
+  $('.hero-slider').slick({
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      adaptiveHeight: true,
+      dotsClass: 'hero-dots',
+      focusOnSelect: true
+    });
+    
+    
+  });
+    
