2f02ac27124f565b195965b00799bfdaf7779d0b
AlexQuidditch <alexquidditch@gmail.com>
2019-07-14 15:55:13 +0300

    - fix slick

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,20 @@
 $(document).ready(function () {
-      
+
   $('.hero-slider').slick({
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear',
-      adaptiveHeight: true,
-      dotsClass: 'hero-dots',
-      focusOnSelect: true
-    });
-  $('.feedback-slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
     dots: true,
     arrows: false,
     infinite: true,
+    speed: 500,
     fade: true,
-    dotsClass: 'feedback-dots',
-    focusOnSelect: true,
-    adaptiveHeight: true
+    cssEase: 'linear',
+    adaptiveHeight: true,
+    dotsClass: 'hero-dots',
+    focusOnSelect: true
   });
-    
+
+  $('.feedback-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
   });
-    
+
+});
