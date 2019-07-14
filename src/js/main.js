297d03d904edba6460baba39c6dc4e478fdd3348
LeloushLamberg <egor.bil.1987@yandex.ru>
2019-07-15 02:25:12 +0800

    PP news

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,17 @@ $(document).ready(function () {
 
   $('.feedback-slider').slick({
     slidesToShow: 2,
-    slidesToScroll: 2,
+    slidesToScroll: 1,
+    accessibility: false,
+    adaptiveHeight: false,
+    autoplay: true,
+    autoplaySpeed: 2500, 
+    arrows: false,
+    dots: true,
+    dotsClass: 'slick-dots',
+    focusOnSelect: true,
+    infinite: true,
+    adaptiveHeight: true
   });
 
 });
