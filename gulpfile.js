ee57847a2e945082f924b204f0c36c290f16da05
LeloushLamberg <egor.bil.1987@yandex.ru>
2019-07-23 15:32:29 +0800

    refresh gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const htmlmin = require('gulp-htmlmin');
 const tinyPNG = require('gulp-tinypng-compress');
 
 
-gulp.task('minify-css', function(done){
+gulp.exports('minify-css', function(done){
     gulp.src('./src/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css/'))
   done()
 });
-gulp.task('minify-js', function(done){
+gulp.exports('minify-js', function(done){
     gulp.src('./src/js/*.js')
     .pipe(minify({
       ignoreFiles: ['*.min.js']
@@ -19,7 +19,7 @@ gulp.task('minify-js', function(done){
     .pipe(gulp.dest('dist/js/'))
   done()
 });
-gulp.task('minify-html', function(done){
+gulp.exports('minify-html', function(done){
     gulp.src('./src/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true
@@ -27,12 +27,12 @@ gulp.task('minify-html', function(done){
     .pipe(gulp.dest('dist/'))
   done()
 });
-gulp.task('move-fonts', function(done){
+gulp.exports('move-fonts', function(done){
   gulp.src('./src/fonts/**/*')
   .pipe(gulp.dest('dist/fonts/'))
 done()
 });
-gulp.task('tinypng', function (done) {
+gulp.exports('tinypng', function (done) {
   gulp.src('./src/img/*/*.{png,jpg,jpeg}')
        .pipe(tinyPNG({
            key: 'D47E7DbIJxCb8zKUnafAgiZKFBjOXMC4'
@@ -40,7 +40,7 @@ gulp.task('tinypng', function (done) {
       .pipe(gulp.dest('dist/img/'));
       done()
 });
-gulp.task('default', gulp.series('minify-css','minify-js', 'minify-html', 'move-fonts', 'tinypng', function (done){
+gulp.exports('default', gulp.series('minify-css','minify-js', 'minify-html', 'move-fonts', 'tinypng', function (done){
   
   done();
 }));
